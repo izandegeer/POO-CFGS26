2c668160c36298aafa7c98c38ec4af02278ec1a2
Izan de Geer <izandegeer.ua@gmail.com>
2026-02-03 20:24:19 +0100

    ++

---
+++ Unidad5/EjerciciosUD5/src/main/java/org/example/insti/InstiApp.java
@@ -0,0 +1,11 @@
+package org.example.insti;
+
+public class InstiApp {
+    static void main() {
+//  Instituto figuerasPachecho = new Instituto("Figueras Pacheco", "Alicante");
+        Instituto figueras = new Instituto();
+        Instituto lloixa = new Instituto("Ies Lloixa", "San Juan");
+        lloixa.setPoblacion(null);
+        
+    }
+}
---
+++ Unidad5/EjerciciosUD5/src/main/java/org/example/insti/Instituto.java
@@ -0,0 +1,20 @@
+package org.example.insti;
+
+import lombok.*;
+
+import java.util.Objects;
+
+@Getter @Setter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Instituto {
+
+    @NonNull
+    private String nombre;
+    private String poblacion;
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = Objects.requireNonNullElse(poblacion, "La población no puede ser nulo");
+    }
+}
